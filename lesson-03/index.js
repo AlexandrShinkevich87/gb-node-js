2db29e50353f4b956a65be70f4d0b23c891e7745
a.shinkevich <a.shinkevich@platiza.ru>
2022-02-16 01:13:51 +0600

    lesson-03 - review

--- lesson-03/index.js
+++ lesson-03/index.js
@@ -4,6 +4,9 @@ const readline = require('readline');
 const ACCESS_LOG = './access-small.log';
 const IP = ['34.48.240.111', '89.123.1.41'];
 
+const ws = IP.map(ip => fs.createWriteStream(`${ip}_requests.log`, {flags: 'a', encoding: 'utf8'}));
+// console.log(ws);
+
 const readStream = fs.createReadStream(
     ACCESS_LOG,
     {
@@ -16,22 +19,29 @@ const rl = readline.createInterface({
 });
 
 readStream.on('open', () => {
-    console.log(new Date()+ '  File opened!');
+    console.log(new Date() + '  File opened!');
 });
 readStream.on('end', () => {
-    console.log(new Date()+'  Finished!');
+    ws.forEach(item => item.end());
+    clearInterval(message);
+    console.log('Обработка файла закончена!');
 });
+
+let i = 1;
+const message = setInterval(() => {
+    console.log('Выполняется обработка файла' + '.'.repeat(i++));
+    i > 20 ? i = 1 : i;
+}, 1000);
+
 readStream.on('error', (err) => {
     console.log(err);
 });
 
-IP.forEach(ip => {
-    rl.on('line', (logLine) => {
-        if (logLine.includes(ip)) {
-            const writeStream = fs.createWriteStream(`${ip}_requests.log`, {
-                flags: 'a',
-            });
-            writeStream.write(`${logLine}\n`);
+rl.on('line', (logline) => {
+    let logLine = logline;
+    ws.forEach((wsItem, index) => {
+        if (logLine.includes(IP[index])) {
+            wsItem.write(`${logLine}\n`);
         }
-    })
-})
+    });
+});
